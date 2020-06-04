cebe97274f836d8a9b4a07a4a5fccd9d0f23df2c
jsliang <1741020489@qq.com>
2020-06-04 09:14:27 +0800

    change

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-const jq = require('jq');
-console.log(jq);
+const str = 'abc123def456abc123';
+const reg = /\d+/g;
+
+// 1. test
+console.log(reg.test(str)); // true
+console.log(reg.lastIndex); // 6
+
+// 2. exec
+console.log(reg.exec(str));
+console.log(reg.lastIndex); // 12
+/**
+  [
+    '456',
+    index: 9,
+    input: 'abc123def456abc123',
+    groups: undefined,
+  ]
+ */
+console.log(reg.exec(str));
+console.log(reg.lastIndex); // 18
+/**
+  [
+    '123',
+    index: 15,
+    input: 'abc123def456abc123',
+    groups: undefined,
+  ]
+ */
