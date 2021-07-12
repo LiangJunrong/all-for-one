9f8b0ccdb7b2fd94dff6075aa94cbeb600aeeab7
jsliang <1741020489@qq.com>
2021-07-12 13:19:46 +0800

    feat: 补充导入流程(含 bug）#master_0712

--- 032-Node 工具库/src/common/language/import.ts
+++ 032-Node 工具库/src/common/language/import.ts
@@ -1,6 +1,7 @@
 import xlsx from 'node-xlsx';
 import fs from 'fs';
 import path from 'path';
+import { language } from './source';
 
 export const importLanguage = async (): Promise<boolean> => {
   const workSheetsFromBuffer = xlsx.parse(
@@ -8,7 +9,7 @@ export const importLanguage = async (): Promise<boolean> => {
       path.join(__dirname, '/dist/Excel 试用文件.xlsx'),
     ),
   );
-  console.log(workSheetsFromBuffer[0].data);
+  const sheet1Data = workSheetsFromBuffer[0].data;
   /**
    * TODO: 根据资源进行导入
   [
@@ -16,6 +17,29 @@ export const importLanguage = async (): Promise<boolean> => {
     [ 'noMoney', '我没钱啦！', 'I have no money', '我沒錢啦！', '我冇钱啦！' ]
   ]
    */
+
+  // 获取头部数据
+  const header = sheet1Data[0];
+  
+  const languageIndex = {};
+  let keyIndex;
+  for (let i = 0; i < header.length; i++) {
+    // 查找 key 对应列
+    if (header[i] === 'key') {
+      keyIndex = i;
+    } else {
+      // 查找其他语言对应列
+      languageIndex[header[i]] = i;
+    }
+  }
+  if (keyIndex !== undefined && keyIndex < 0) {
+    console.error('未找到 key 对应列！');
+    return false;
+  }
+
+  const importData = {};
+  
+  console.log(language);
 
   return await true;
 };
