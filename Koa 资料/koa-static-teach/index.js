b74258321e8cf14ed32a32e04b052258e7739303
jsliang <1741020489@qq.com>
2023-01-08 00:27:22 +0800

    delete: 删除无用资料

--- Koa 资料/koa-static-teach/index.js
+++
@@ -1,9 +0,0 @@
-const serve = require('koa-static');
-const Koa = require('koa');
-const app = new Koa();
-
-// or use absolute paths
-app.use(serve(__dirname + '/static',{
-    index: "main.html"
-}));
-app.listen(9001);
