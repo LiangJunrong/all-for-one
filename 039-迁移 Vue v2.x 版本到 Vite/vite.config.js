47fc00b4f51accd96e710394c5ae6bce29bd6eb2
jsliang <1741020489@qq.com>
2023-01-30 08:51:00 +0800

    迁移 Vue v2.x 版本到 Vite

--- 039-迁移 Vue v2.x 版本到 Vite/vite.config.js
+++ 039-迁移 Vue v2.x 版本到 Vite/vite.config.js
@@ -12,21 +12,35 @@ export default defineConfig({
   // 打包模式
   build: {
     // 库模式
-    lib: {
-      // 设置入口文件
-      entry: {
+    // lib: {
+    //   // 设置入口文件
+    //   entry: {
+    //     'A': 'src/components/a/entry.js',
+    //     'B': 'src/components/b/entry.js'
+    //   },
+    //   formats: ['es'],
+    //   // 打包后的文件名
+    //   fileName: (format, entryName) => `${entryName}/${entryName}.entry.${format}.js`,
+    // },
+    rollupOptions: {
+      input: {
         'A': 'src/components/a/entry.js',
         'B': 'src/components/b/entry.js'
       },
-      formats: ['es'],
-      // 打包后的文件名
-      fileName: (format, entryName) => `${entryName}/${entryName}.entry.${format}.js`,
+      output: [
+        {
+          dir: 'dist1',
+          // file: 'dist/a/a.js',
+        },
+        {
+          dir: 'dist2',
+          // file: 'dist/b/b.js',
+        }
+      ]
+      // output: {
+      //   dir: 'dist/',
+      // }
     },
-    // rollupOptions: {
-    //   output: {
-
-    //   }
-    // }
   }
 });
 
